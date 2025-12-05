f9c63a5b653bbf638598dc4718265bb8d481c382
Leo Denizen <leo@denizen-family.com>
2025-12-05 11:49:15 -0800

    interfacing

--- postOffice.java
+++ postOffice.java
@@ -1,7 +1,111 @@
 /**
  * Docs
  */
-public class postOffice
+public class postOffice implements heapUtil
 {
-    
+    @Override
+    public void heapify(Comparable[] heap, int index, int heapSize)
+    {
+        int leftind = 2 * index;
+        int rightind = leftind + 1;
+        int top = index;
+        if (leftind <= heapSize && heap[leftind] != null &&
+            heap[top].compareTo(heap[leftind]) < 0)
+        {
+            top = leftind;
+        }
+        if (rightind <= heapSize && heap[rightind] != null &&
+            heap[top].compareTo(heap[rightind]) < 0)
+        {
+            top = rightind;
+        }
+        if (top != index)
+        {
+            Comparable temp = heap[index];
+            heap[index] = heap[top];
+            heap[top] = temp;
+            index = top;
+            heapify(heap, index, heapSize);
+        }
+    }
+    @Override
+    public void buildHeap(Comparable[] heap, int heapSize)
+    {
+        for (int i = heapSize / 2; i >= 1; i--)
+        {
+            for(int j = 1; j<heap.length; j++)
+            {
+                System.out.print(heap[j] + " ");
+            }
+            heapify(heap, i, heapSize);
+        }
+        for(int j = 1; j<heap.length; j++)
+        {
+            System.out.print(heap[j] + " ");
+        }
+        System.out.println();
+    }
+    @Override
+    public Comparable remove(Comparable[] heap, int heapSize)
+    {
+        if (heapSize < 1) 
+        {
+            return null;
+        }
+        Comparable removed = heap[1];
+        heap[1] = heap[heapSize];
+        heap[heapSize] = null;
+        heapSize--;
+        heapify(heap, 1, heapSize);
+        return removed;
+    }
+    @Override
+    public Comparable[] insert(Comparable[] heap, Comparable item, int heapSize)
+    {
+        heapSize++;
+        if (heapSize >= heap.length)
+        {
+            Integer[] bigger = new Integer[heap.length * 2];
+            for (int i = 0; i < heap.length; i++)
+            {
+                bigger[i] = (Integer)heap[i];
+            }
+            heap = bigger;
+        }
+        System.out.println(heapSize);
+        System.out.println(heap.length);
+        heap[heapSize] = item;
+        int index = heapSize;
+        boolean escape = false;
+        while (index > 1 && !escape)
+        {
+            int parent = index / 2;
+            if (heap[parent].compareTo(heap[index]) >= 0)
+            {
+                escape = true;
+            }
+            else
+            {
+                Comparable temp = heap[parent];
+                heap[parent] = heap[index];
+                heap[index] = temp;
+                index = parent;
+            }
+        }
+        return heap; 
+    }
+    @Override
+    public Comparable[] heapSort(Comparable[] heap, int heapSize)
+    {
+        buildHeap(heap, heapSize);
+        while (heapSize > 1)
+        {
+            Comparable temp = heap[1];
+            heap[1] = heap[heapSize];
+            heap[heapSize] = temp;
+            heapSize--;
+            heapify(heap, 1, heapSize);
+        }
+        return heap;
+    }
 }
